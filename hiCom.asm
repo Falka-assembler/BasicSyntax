744285274bffd93ecf8f884b1aff7d27efb39ff3
Владислав Швец <vsh15975@gmail.com>
2022-03-03 17:23:23 +0200

    Changed hiCom

--- hiCom.asm
+++ hiCom.asm
@@ -1,12 +1,13 @@
 .8086
-code SEGMENT para USE16 PUBLIC 'code'
-    ASSUME CS:code, DS:code, SS:code, ES:code
-    ORG 100h
+.model tiny
+
+code segment
+    org 100h
     START:
-    message db 'Hello World$'
-    mov ah, 9h
-    mov dx, offset message
-    int 21h
-    ret
+        mov ah,9h
+        mov dx, offset message
+        int 21h
+        int 20h
+        message db 'Hello world!$'
 code ends
 end START
---
+++ test.asm
@@ -0,0 +1,8 @@
+.8086
+.model tiny
+code segment
+    org 100h
+    START:
+    
+code ends
+end START
